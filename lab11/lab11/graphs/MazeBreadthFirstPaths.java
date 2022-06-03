9ebfdfc825fa41c6435c80180f852860ea7147a5
victory-hlx <a1272012007@163.com>
2022-06-03 15:10:56 +0800

    finished lab 11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +12,72 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        marked[s] = true;
+
+
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
-    }
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
+    private void bfs(int v) {
+        // TODO: Your code here. Don't forget to update distTo, edgeTo,
+        //  and marked, as well as call announce()
 
+        Queue<Integer> q = new PriorityQueue<>();
+
+        marked[v] = true;
+        q.add(v);
+        announce();
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+
+        while (!q.isEmpty()) {
+            int m = q.remove();
+            for (int w : maze.adj(m)) {
+                if (!marked[w]) {
+                    edgeTo[w] = m;
+                    announce();
+                    distTo[w] = distTo[m] + 1;
+                    marked[w] = true;
+                    q.add(w);
+                    if (w == t) {
+                        targetFound = true;
+                    }
+
+                    if (targetFound) {
+                        return;
+                    }
+                }
+            }
+        }
+
+
+    }
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
